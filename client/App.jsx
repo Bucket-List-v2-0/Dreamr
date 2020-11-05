769c45746ecd87d4ef1c4697452267a9dd349a22
Kim Mai Nguyen <kim.mai.e.nguyen@gmail.com>
2020-11-04 17:29:14 -0700

    back end done

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Home from './components/Home.jsx';
+// import Login from './components/Login.jsx';
+
+function App() {
+	return (
+		<div className="App">
+			<a href="/auth/google">Sign In with Google</a>
+		</div>
+	);
+}
+
+export default App;
--- client/src/components/Feed.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const Feed = () => {
-  return <div>Hello Feed</div>;
-};
-
-export default Feed;
